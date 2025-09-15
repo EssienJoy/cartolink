db74d3002e299c498c9874499ac0a6a752ff93f2
essienjoy <ejoy7602@gmail.com>
2025-09-15 07:52:37 +0100

    changed slider button size for easier navigation

--- src/app/_components/HeroSliderBtn.jsx
+++ src/app/_components/HeroSliderBtn.jsx
@@ -7,12 +7,12 @@ import { useSlider } from "@/app/_components/SliderContext";
 function HeroSliderBtn() {
 	const { increaseCurSlide, decreaseCurSlide } = useSlider();
 	return (
-		<aside className='flex gap-2 '>
+		<aside className='flex gap-4 '>
 			<Button className='p-1 rounded-2xl' onClick={decreaseCurSlide}>
-				<IoIosArrowBack />
+				<IoIosArrowBack size='1.35rem' />
 			</Button>
 			<Button className='p-1 rounded-2xl' onClick={increaseCurSlide}>
-				<IoIosArrowForward />
+				<IoIosArrowForward size='1.35rem' />
 			</Button>
 		</aside>
 	);
